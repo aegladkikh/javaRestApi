a9660bd86f0db7315346bd57b0791f9723d72866
Slip <tolikslip@mail.ru>
2020-03-01 00:12:17 +0700

    add some grud

---
+++ src/main/java/slip/demo/accessingdatamysql/UserRepository.java
@@ -0,0 +1,12 @@
+package slip.demo.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import slip.demo.accessingdatamysql.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
